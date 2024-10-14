437969ae6a71e56bf3a3ab872bb1db1ea92b27c2
Milen Tecle <m_tecle@hotmail.com>
2024-10-14 11:58:21 +0000

    set up the routing and add the linking for the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import styles from "./App.module.css";
 import NavBar from './components/NavBar';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home page</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign in</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
