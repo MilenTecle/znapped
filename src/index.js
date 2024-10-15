48cb65bc710c37a3a29167b5ae37c3e4557d944d
Milen Tecle <m_tecle@hotmail.com>
2024-10-15 12:31:50 +0000

    add remaining icons to the Navbar and add logout functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { CurentUserProvider } from './contexts/CurrentUserContext';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <CurentUserProvider>
+      <CurrentUserProvider>
         <App />
-      </CurentUserProvider>
-
+      </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,12 +15,9 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
-import { useRedirect } from "../../hooks/useRedirect";
-import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
-  useRedirect("loggedIn");
 
   const [signInData, setSignInData] = useState({
     username: "",
@@ -37,8 +34,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      setTokenTimestamp(data);
-      history.goBack();
+      history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
     }
