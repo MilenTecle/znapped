9f53681449aafcd5e56d038423d0acd7e4d5103f
Milen Tecle <m_tecle@hotmail.com>
2024-10-21 19:17:40 +0000

    create PostEditForm so user can edit their posts

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,6 +1,6 @@
-import React from "react"
-import Dropdown from 'react-bootstrap/Dropdown'
-import styles from "../styles/MoreDropdown.module.css"
+import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../styles/MoreDropdown.module.css";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
@@ -23,7 +23,7 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
       <Dropdown.Menu
         className="text-center"
         popperConfig={{ strategy: "fixed" }}
-        >
+      >
         <Dropdown.Item
           className={styles.DropdownItem}
           onClick={handleEdit}
@@ -33,10 +33,10 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
         </Dropdown.Item>
         <Dropdown.Item
           className={styles.DropdownItem}
-          oncClick={handleDelete}
+          onClick={handleDelete}
           aria-label="delete"
-          >
-            <i className="fas fa-trash-alt" />
+        >
+          <i className="fas fa-trash-alt" />
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
